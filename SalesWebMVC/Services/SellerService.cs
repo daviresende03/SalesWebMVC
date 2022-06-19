6b0c021140af4db344073cd8c1a843b05c662774
daviresende03 <daviresendedev@gmail.com>
2022-06-19 20:50:35 -0300

    SellerService and FindAll

---
+++ SalesWebMVC/Services/SellerService.cs
@@ -0,0 +1,22 @@
+﻿using SalesWebMVC.Data;
+using SalesWebMVC.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMVC.Services
+{
+    public class SellerService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SellerService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
